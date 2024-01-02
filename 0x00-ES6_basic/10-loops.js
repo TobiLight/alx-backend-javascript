ccc12860e6f63ea7ce2519ed673dc6441ddd061d
TobiLight <jgetitdonefast@gmail.com>
2024-01-02 20:27:00 +0100

    ✔ Uses ES6’s for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
-  for (const [i, value] of array.entries()) {
-    newArray[i] = appendString + value;
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
   return newArray;
