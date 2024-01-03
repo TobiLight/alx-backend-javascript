0e733e81d60afc77c106ecd1f14749e766796296
TobiLight <jgetitdonefast@gmail.com>
2024-01-03 19:22:21 +0100

    ✔ Returns an array of resolved promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photo = uploadPhoto(fileName);
 
   return Promise.allSettled([user, photo])
-    .then(([userData, photoData]) => [userData, photoData])
+    .then(([userData, photoData]) => [userData, photoData]);
 }
