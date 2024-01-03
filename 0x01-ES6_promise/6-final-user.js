7b9c11555a89c1d7ac3efc1e51effc33eaf6481c
TobiLight <jgetitdonefast@gmail.com>
2024-01-03 21:57:35 +0100

    ✔ Returns an array of resolved promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-
-  return Promise.allSettled([user, photo])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => (result.map((res) => ({
       status: res.status,
       value: res.status === 'fulfilled' ? res.value : res.reason,
