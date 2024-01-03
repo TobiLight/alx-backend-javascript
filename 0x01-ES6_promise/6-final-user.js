2439760f6c3886697c6b7aa723ee26bf8548b3fb
TobiLight <jgetitdonefast@gmail.com>
2024-01-03 19:13:29 +0100

    ✔ Returns an array of resolved promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.all([user, photo])
+    .then(([userData, photoData]) => [...userData, ...photoData])
+    .catch(() => { });
+}
