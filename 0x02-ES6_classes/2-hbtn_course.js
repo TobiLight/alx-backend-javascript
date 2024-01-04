067c4a9a600b82a9036a68a3d1ae54cfbc721e95
TobiLight <jgetitdonefast@gmail.com>
2024-01-04 20:41:57 +0100

    ✔ Implements a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = this._validateName(name);
+    this._length = this._validateLength(length);
+    this._students = this._validateStudents(students);
   }
 
   get name() {
