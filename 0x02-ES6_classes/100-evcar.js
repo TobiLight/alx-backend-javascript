d3ed2ab59edf60e785704660aa4a2edbada72a56
TobiLight <jgetitdonefast@gmail.com>
2024-01-04 23:06:37 +0100

    ✔ Implements a class named EVCar that extends Car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Clone = super.constructor[Symbol.species];
+    return new Clone();
+  }
+}
