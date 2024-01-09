238ce8faaff2b76c1cdc189dea013fd3e440fd37
TobiLight <jgetitdonefast@gmail.com>
2024-01-09 21:42:24 +0100

    ✔ Returns a new ArrayBuffer with an Int8 value at a specific position.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,9 +4,9 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
 
-  int8[position] = value;
+  dataView.setInt8(position, value);
 
-  return buffer;
+  return dataView;
 }
