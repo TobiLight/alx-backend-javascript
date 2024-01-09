d959269f13bd5d1240f1e9ec694f31650e1e71ca
TobiLight <jgetitdonefast@gmail.com>
2024-01-09 23:16:34 +0100

    ✔ Tracks the number of times queryAPI is called for each endpoint.

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   const counter = (weakMap.get(endpoint) || 0) + 1;
-  if (counter >= 5) throw new Error('Endpoint load is high')
+  if (counter >= 5) throw new Error('Endpoint load is high');
   weakMap.set(endpoint, counter);
 }
 
