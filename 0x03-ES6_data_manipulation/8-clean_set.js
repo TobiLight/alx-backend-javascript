21328c98a3386e22871c1820445785cdba25bfcf
TobiLight <jgetitdonefast@gmail.com>
2024-01-09 22:42:15 +0100

    ✔ Returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || !(set instanceof Set) || !set) {
+  if (!startString || !startString.length || typeof startString !== 'string' || !(set instanceof Set) || !set) {
     return '';
   }
 
   const arrayFromSet = Array.from(set);
-  const filteredArr = arrayFromSet.filter((item) => item.startsWith(startString));
+  const filteredArr = arrayFromSet.filter((item) => item && item.startsWith(startString));
   const cleanedVal = filteredArr.map((item) => item.slice(startString.length));
   return cleanedVal.join('-');
 }
