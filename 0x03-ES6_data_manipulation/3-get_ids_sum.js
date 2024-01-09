7c7fb5d52742c9df16eb993bbf035dc647ebd1fa
TobiLight <jgetitdonefast@gmail.com>
2024-01-09 20:18:10 +0100

    ✔ Returns the sum of all the student ids.

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,8 @@
 import getListStudentIds from './1-get_list_student_ids';
 
 function getStudentIdsSum(studentsArr) {
-	const ids = getListStudentIds(studentsArr);
-	return ids.reduce((accumulator, id) => accumulator + id);
+  const ids = getListStudentIds(studentsArr);
+  return ids.reduce((accumulator, id) => accumulator + id);
 }
 
 export default getStudentIdsSum;
