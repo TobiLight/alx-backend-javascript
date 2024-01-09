f32cb2d3466a29944c6f6137e2d544973865488f
TobiLight <jgetitdonefast@gmail.com>
2024-01-09 21:26:20 +0100

    ✔ Returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,13 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 function updateStudentGradeByCity(studentList, city, newGrades) {
-  const students = getStudentsByLocation(studentList, city);
-
-  return students.map((student) => {
-    const matchingGrades = newGrades.find((stud) => stud.studentId === student.id);
-    return {
-      ...student,
-      grade: matchingGrades ? matchingGrades.grade : 'N/A',
-    };
-  });
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrades = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: matchingGrades ? matchingGrades.grade : 'N/A',
+      }
+    });
 }
 
 export default updateStudentGradeByCity;
