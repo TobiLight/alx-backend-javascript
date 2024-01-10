3143cc4024f8718c47ca5a4b0132cf01feedbaae
TobiLight <jgetitdonefast@gmail.com>
2024-01-10 19:00:43 +0100

    ✔ Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,13 +1,13 @@
 export interface DirectorInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string
-	workDirectorTasks(): string
+	workFromHome: () => string;
+	getCoffeeBreak: () => string
+	workDirectorTasks: () => string
 }
 
 export interface TeacherInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string
-	workTeacherTasks(): string
+	workFromHome: () => string;
+	getCoffeeBreak: () => string
+	workTeacherTasks: () => string
 }
 
 export class Director implements DirectorInterface {
@@ -43,4 +43,16 @@ export function createEmployee(salary: number | string): Teacher | Director {
 		return new Teacher();
 	}
 	return new Director();
+}
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+	return employee instanceof Director
+}
+
+export function executeWork(employee: Director | TeacherInterface): string {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	}
+
+	return (employee as Teacher).workTeacherTasks();
 }
