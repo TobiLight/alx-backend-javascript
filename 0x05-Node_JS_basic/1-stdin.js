2a11c02c3d6fd33e1c160da6954888ebe021cd16
TobiLight <jgetitdonefast@gmail.com>
2024-02-26 21:35:50 +0100

    ✔ fix: Semistandard error fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,27 +3,27 @@
  * Author: Oluwatobiloba Light
  */
 
-const rl = require('readline')
+const rl = require('readline');
 
 const readline = rl.createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout
+});
 
 const displayWelcoeMessage = () => {
-	return 'Welcome to Holberton School, what is your name?\n'
-}
+  return 'Welcome to Holberton School, what is your name?\n';
+};
 
 const askName = () => {
-	readline.question(displayWelcoeMessage(), answer => {
-		console.log(`Your name is ${answer}`)
-		endProgram()
-	})
-}
+  readline.question(displayWelcoeMessage(), answer => {
+    console.log(`Your name is ${answer}`);
+    endProgram();
+  });
+};
 
 const endProgram = () => {
-	console.log('This important software is now closing')
-	readline.close()
-}
+  console.log('This important software is now closing');
+  readline.close();
+};
 
-askName()
+askName();
