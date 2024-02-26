03cf66b0140777330ab331f6e634b3319f5de3cc
TobiLight <jgetitdonefast@gmail.com>
2024-02-26 15:03:04 +0100

    ✔ Prints the given message as argument to the console.

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,16 @@
+/**
+ * File: 0-console.js
+ * Author: Oluwatobiloba Light
+ * Description: Prints in STDOUT the string argument.
+ */
+
+/**
+ * Prints the given message to the console.
+ *
+ * @param {string} message - The message to print.
+ */
+const displayMessage = (message) => {
+	console.log(message)
+}
+
+module.exports = displayMessage
