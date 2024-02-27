114ab7683a98393917765ec23e9d5e0af402ce10
TobiLight <jgetitdonefast@gmail.com>
2024-02-28 00:15:54 +0100

    ✔ Basic HTTP server that displays a plain text on any route

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+/**
+ * Displays Hello Holberton School! in the page body for any endpoint as plain text
+ *
+ * @author - Oluwatobiloba Light
+ */
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
