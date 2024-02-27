b728a550703e724e2af02f28abe27976f4d929e9
TobiLight <jgetitdonefast@gmail.com>
2024-02-27 23:48:17 +0100

    ✔ Function that asynchronously reads a file

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+/**
+ * Counts the number of students and students in each field from a CSV file.
+ *
+ * This function asynchronously reads the provided CSV file and processes the data
+ * to count the total number of students and the number of students in each field.
+ * It also generates a list of unique values for each field.
+ *
+ * @param {string} filePath - The path to the CSV file containing student data.
+ *
+ * @throws {Error} - If the file cannot be read or is empty.
+ * @author - Oluwatobiloba Light
+ */
+const countStudents = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const list = data.toString().trim().split('\n');
+
+      const studentGroups = {};
+      const dbFieldNames = list[0].split(',');
+      const studentInfo = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+      for (let i = 1; i < list.length; i++) {
+        const studentRecords = list[i].split(',');
+        const studentPropVal = studentRecords.slice(0, studentRecords.length - 1);
+        const field = studentRecords[studentRecords.length - 1];
+
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
+        }
+
+        const entries = studentInfo.map((student, idx) => [student, studentPropVal[idx]]);
+        studentGroups[field].push(Object.fromEntries(entries));
+      }
+
+      const totalStudents = Object.values(studentGroups).reduce((pre, curr) => (pre || []).length + curr.length);
+
+      console.log(`Number of students: ${totalStudents}`);
+
+      for (const [key, value] of Object.entries(studentGroups)) {
+        const names = value.map(student => student.firstname).join(', ');
+        console.log(`Number of students in ${key}: ${value.length}. List: ${names}`);
+      }
+      resolve();
+    }
+  });
+});
+
+module.exports = countStudents;
