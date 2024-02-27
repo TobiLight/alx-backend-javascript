6082ddab2bbcc6fcbbdb2b318a1c80833f455974
TobiLight <jgetitdonefast@gmail.com>
2024-02-27 02:52:03 +0100

    ✔ fix: Semistandard error fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,10 +5,13 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', answer => {
-  process.stdout.write(`Your name is: ${answer}`);
+process.stdin.on('readable', () => {
+  const answer = process.stdin.read();
+  if (answer) {
+    process.stdout.write(`Your name is: ${answer}`);
+  }
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
