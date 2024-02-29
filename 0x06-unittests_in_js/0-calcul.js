f8068c7ea8916af6d6815b2b119033b1b4be82b1
TobiLight <jgetitdonefast@gmail.com>
2024-02-29 19:12:11 +0100

    ✔ Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,12 @@
+// Author: Oluwatobiloba Light
+
+/**
+ * Returns the sum of 2 integers
+ * @param {integer} a - First argument
+ * @param {integer} b - Second argument
+ * @returns {integer} - The sum of a and b
+ */
+
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe('calculateNumber', () => {
+  it('should return the sum of two numbers', () => {
+    const result = calculateNumber(3, 5);
+    assert.equal(result, 8);
+  })
+
+  it('should return the sum of a number and rounded number', () => {
+    const result = calculateNumber(3, 3.5);
+    assert.equal(result, 7);
+  })
+
+  it('should return the sum of rounded numbers', () => {
+    const result = calculateNumber(1.2, 3.7);
+    assert.equal(result, 5);
+  })
+
+  it('should handle positive and negative decimals', () => {
+    const result = calculateNumber(-2.5, 4.2);
+    assert.equal(result, 2);
+  });
+
+  it('should return 0 for zero inputs', () => {
+    const result = calculateNumber(0, 0);
+    assert.equal(result, 0);
+  });
+})
