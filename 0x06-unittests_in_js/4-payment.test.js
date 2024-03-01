de10c334b4e512aaa6e3ac9b613172d22235bb42
TobiLight <jgetitdonefast@gmail.com>
2024-03-01 01:00:35 +0100

    ✔ This module contains test cases for the sendPaymentRequestToApi function using spy and stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+// Author: Oluwatobiloba Light
+
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require('./utils');
+const sinon = require('sinon')
+
+/**
+ * @module 3-payment.test
+ * @description This module contains test cases for the sendPaymentRequestToApi function.
+ */
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const consoleSpy = sinon.spy(console);
+    const apiStub = sinon.stub(Utils);
+
+    apiStub.calculateNumber.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(apiStub.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
+    expect(apiStub.calculateNumber.calledOnce).to.be.true;
+    expect(apiStub.calculateNumber.callCount).to.be.equal(1);
+
+    expect(consoleSpy.log.calledWith('The total is: 10')).to.be.true;
+    expect(consoleSpy.log.calledOnce).to.be.true;
+    expect(consoleSpy.log.callCount).to.be.equal(1);
+
+    apiStub.calculateNumber.restore();
+    consoleSpy.log.restore();
+  })
+
+})
